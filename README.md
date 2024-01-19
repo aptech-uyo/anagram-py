76e9d6bc701b049b839d59991695b6a9e138cc33
Aptech Computer Education, Uyo <contact@aptech-uyo.info>
2024-01-19 13:04:11 +0100

    reorganize tests

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ An **anagram** is the result of rearranging the letters of a word to produce a n
 
 **Note:** Anagrams are case insensitive
 
-Complete the function to return `true` if the two arguments given are anagrams of each other; return `false` otherwise.
+Complete the `is_anagram` function to return `true` if the two arguments given are anagrams of each other; return `false` otherwise.
 
 ### Examples
 `"foefet"` is an anagram of `"toffee"`
--- tests.py
+++ tests.py
@@ -3,12 +3,22 @@ from solution import is_anagram
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_cases(self):
+    def test1(self):
         self.assertEqual(is_anagram('foefet', 'toffee'), True, 'The word foefet is an anagram of toffee')
+
+    def test2(self):
         self.assertEqual(is_anagram('Buckethead', 'DeathCubeK'), True, 'The word Buckethead is an anagram of DeathCubeK')
+
+    def test3(self):
         self.assertEqual(is_anagram('Twoo', 'WooT'), True, 'The word Twoo is an anagram of WooT')
+
+    def test4(self):
         self.assertEqual(is_anagram('dumble', 'bumble'), False, 'Characters do not match for test case dumble, bumble')
+
+    def test5(self):
         self.assertEqual(is_anagram('ound', 'round'), False, 'Missing characters for test case ound, round')
+
+    def test6(self):
         self.assertEqual(is_anagram('apple', 'pale'), False, 'Same letters, but different count')
 
 if __name__ == '__main__':
