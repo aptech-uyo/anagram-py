fe023d137dfb10e5efa82b0a9401987f8ed2689c
Hexw4r3 <carlosprosper2@gmail.com>
2024-01-25 10:00:54 -0500

    function to check anagram

--- solution.py
+++ solution.py
@@ -1,3 +1,16 @@
 # write the function is_anagram
 def is_anagram(test, original):
-    pass
+    test = test.lower().strip()
+    original = original.lower().strip()
+
+    if sorted(test) == sorted(original):
+        print("Words are anagrams")
+    else:
+        print("Words are not anagram")
+
+
+
+test = "orange"
+original = "granoe"
+
+is_anagram(test, original)
